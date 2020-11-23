281be12f051536f98405f48a239dc05e7b3f205e
haoyuan.lei <haoyuan.lei@luckincoffee.com>
2020-11-23 11:04:20 +0800

    结构体测试
    Change-Id: Icde628c106e3b57838d7815eb957e982bfd60c51

--- src/main/StructTest.go
+++ src/main/StructTest.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 /*
 1、结构体（struct）将多个不同类型命名字段（field）序列打包成一个复合类型
-
+2、字段名必须唯一，可用“_”补位，支持使用自身指针类型成员
 */
 func main() {
+	n1 := node{
+		id: 1,
+	}
 
+	n2 := node{
+		id:   2,
+		next: &n1,
+	}
+
+	fmt.Println(n1, n2)
+}
+
+type node struct {
+	_    int
+	id   int
+	next *node
 }
